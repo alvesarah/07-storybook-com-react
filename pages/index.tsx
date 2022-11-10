2d5e312a9f66ec186a211bf603b8fb0eaff2d73e
alvesarah <sarah.alves8@hotmail.com>
2022-11-10 04:05:28 -0300

    feat: adiconado ThemeProvider para ser feita a seleção de cor no botão.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,15 @@
+import Button from "../src/components/Button/Button"
+import ThemeProvider from "../src/theme/ThemeProvider"
+
 export default function HomeScreen() {
   return (
-    <div>
-      <h1>Home Page!</h1>
-    </div>
+    <ThemeProvider>
+      <div>
+        <h1>Home Page!</h1>
+        <Button>
+          Botão Simplão
+        </Button>
+      </div>
+    </ThemeProvider>
   )
 }
